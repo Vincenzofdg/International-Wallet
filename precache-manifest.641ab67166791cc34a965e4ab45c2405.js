73ea7ac49d83289182aa85120d85378969685b3b
Vincenzofdg <vfdgiacomo@gmail.com>
2022-05-02 02:57:04 -0300

    Updates

---
+++ precache-manifest.641ab67166791cc34a965e4ab45c2405.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afec971d46d145afd8315722656c03b",
+    "url": "/International-Wallet/index.html"
+  },
+  {
+    "revision": "b4683a1a31f831682813",
+    "url": "/International-Wallet/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "81f280c30a1f56bb0dd4",
+    "url": "/International-Wallet/static/css/main.32d1e723.chunk.css"
+  },
+  {
+    "revision": "b4683a1a31f831682813",
+    "url": "/International-Wallet/static/js/2.99431303.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/International-Wallet/static/js/2.99431303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f280c30a1f56bb0dd4",
+    "url": "/International-Wallet/static/js/main.24b25508.chunk.js"
+  },
+  {
+    "revision": "f95e66dba3d1335d4dc9",
+    "url": "/International-Wallet/static/js/runtime-main.73171e41.js"
+  },
+  {
+    "revision": "63abca6b3ab0841fee92348b88bf8f9d",
+    "url": "/International-Wallet/static/media/wallet.63abca6b.png"
+  },
+  {
+    "revision": "4d04fedc4421135b097bd227a35ba45e",
+    "url": "/International-Wallet/static/media/wave-haikei.4d04fedc.svg"
+  }
+]);
